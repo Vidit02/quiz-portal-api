eb141c15afc93e5fd746239db2c8f22f9401ac11
“Vidit <vidit.21102@gmail.com>
2022-11-09 00:42:53 +0530

    Edit Category DB done

--- controller/category-controller.js
+++ controller/category-controller.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const categoryModel = require("../models/category.model")
 
-function addCategory(req,res){
+function addCategory(req, res) {
     let title = req.body.title
     let description = req.body.description
     let type = req.body.type
@@ -9,51 +9,51 @@ function addCategory(req,res){
     let error = {}
     let isError = false
 
-    if(title == undefined || title == ""){
+    if (title == undefined || title == "") {
         isError = true
         error.titleError = "Please Enter Title"
     }
-    if(description == undefined || description == ""){
+    if (description == undefined || description == "") {
         isError = true
         error.descError = "Please Enter Description"
     }
-    if(type == undefined || type == ""){
+    if (type == undefined || type == "") {
         isError = true
         error.typeError = "Please Enter Type"
     }
-    if(isError){
+    if (isError) {
         res.json({
-            status : 401,
-            data : req.body,
-            msg : "Please Correct Errors",
-            error : error
+            status: 401,
+            data: req.body,
+            msg: "Please Correct Errors",
+            error: error
         })
     } else {
-        categoryModel.findOne({"title" : title},(err,success) => {
-            if(success) {
+        categoryModel.findOne({ "title": title }, (err, success) => {
+            if (success) {
                 res.json({
-                    status : 402,
-                    msg : "Category Already Exist",
-                    data : title
+                    status: 402,
+                    msg: "Category Already Exist",
+                    data: title
                 })
             } else {
                 let category = new categoryModel({
-                    "title" : title,
-                    "description" : description,
-                    "type" : type
+                    "title": title,
+                    "description": description,
+                    "type": type
                 })
-                category.save(function(err,success){
-                    if(err){
+                category.save(function (err, success) {
+                    if (err) {
                         res.json({
-                            status : 403,
-                            msg : "Something went wrong",
-                            data : req.body
+                            status: 403,
+                            msg: "Something went wrong",
+                            data: req.body
                         })
                     } else {
                         res.json({
-                            msg : "Category Saved",
-                            status : 200,
-                            data : success
+                            msg: "Category Saved",
+                            status: 200,
+                            data: success
                         })
                     }
                 })
@@ -62,44 +62,93 @@ function addCategory(req,res){
     }
 }
 
-function listAllCategory(req,res){
-    categoryModel.find().exec((err,success)=>{
-        if(err || err == null && success == null){
+function listAllCategory(req, res) {
+    categoryModel.find().exec((err, success) => {
+        if (err || err == null && success == null) {
             res.json({
-                status : 401,
-                msg : "Something is wrong"
+                status: 401,
+                msg: "Something is wrong"
             })
         } else {
             res.json({
-                status : 200,
-                msg : "Categories present",
-                data : success
+                status: 200,
+                msg: "Categories present",
+                data: success
             })
         }
     })
 }
 
-function getCategory(req,res){
+function getCategory(req, res) {
     let title = req.body.title
     // console.log(title);
-    categoryModel.findOne({"title":title},(err,success)=>{
+    categoryModel.findOne({ "title": title }, (err, success) => {
         console.log(err);
-        if(err){
+        if (err) {
             res.json({
-                status : 401,
-                msg : "Something is Wrong",
-                data : req.body
+                status: 401,
+                msg: "Something is Wrong",
+                data: req.body
             })
         } else {
             res.json({
-                status : 200,
-                data : success,
-                msg : "Category Found"
+                status: 200,
+                data: success,
+                msg: "Category Found"
             })
         }
     })
 }
 
+function updateCategory(req, res) {
+    let title = req.body.title
+    let description = req.body.description
+    let type = req.body.type
+    // category.findOneAndUpdate({ "title": title }, (err, success) => {
+    //     if (err) {
+    //         res.json({
+    //             status: 403,
+    //             msg: "Something went wrong",
+    //             data: req.body
+    //         })
+    //     } else {
+    //         res.json({
+    //             msg: "Category Updated",
+    //             status: 200,
+    //             data: success
+    //         })
+    //     }
+    // })
+    categoryModel.findOne({ "title": title }, (err, success) => {
+        if (err) {
+            res.json({
+                status: 402,
+                msg: "Category Already Exist",
+                data: title
+            })
+        } else {
+            let category ={$set : {"description":description,"type":type}}
+            categoryModel.updateOne({"title" : title},category,(err, success)=> {
+                if (err) {
+                    console.log(err);
+                    res.json({
+                        status: 403,
+                        msg: "Something went wrong",
+                        data: req.body
+                    })
+                } else {
+                    console.log("Category updated",description,type,title);
+                    res.json({
+                        msg: "Category Saved",
+                        status: 200,
+                        data: success
+                    })
+                }
+            })
+        }
+    })
+}
 module.exports.addCategory = addCategory
 module.exports.listAllCategory = listAllCategory
 module.exports.getCategory = getCategory
+module.exports.updateCategory = updateCategory
--- routes/quiz-routes.js
+++ routes/quiz-routes.js
@@ -4,6 +4,7 @@ var categoryController = require("../controller/category-controller")
 router.post("/addcategory",categoryController.addCategory)
 router.get("/listcategory",categoryController.listAllCategory)
 router.post("/getcategory",categoryController.getCategory)
+router.post("/updatecategory",categoryController.updateCategory)
 
 
 module.exports = router
